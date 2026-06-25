7c41e7faba414682742849849519e565124cfb4d
Hoangho412 <Hoanghoa0420@gmail.com>
2026-06-25 10:05:30 +0700

    fix foodDAO

--- src/main/java/com/mbcms/dao/impl/FoodDAOImpl.java
+++ src/main/java/com/mbcms/dao/impl/FoodDAOImpl.java
@@ -56,12 +56,7 @@ public class FoodDAOImpl extends BaseDAO implements FoodDAO {
             ps.setLong(1, foodId);
             rs = ps.executeQuery();
             if (rs.next()) {
-<<<<<<< HEAD
-            return mapRow(rs);
-
-=======
                 return mapRow(rs);
->>>>>>> main
             }
             return null;
         } catch (SQLException e) {
